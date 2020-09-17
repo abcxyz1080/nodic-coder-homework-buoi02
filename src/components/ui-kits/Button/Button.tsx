8c7f3f18d60f6353f431fcdd92691f5ad17d1c37
Hùng <hung@vsmi.network>
2020-09-17 16:25:08 +0700

    first commit

---
+++ src/components/ui-kits/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledButton } from './Button.styled'
+
+interface ButtonProps {
+  children: React.ReactChild
+  onClick?(e: React.MouseEvent<HTMLElement>): void
+  className?: string
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return <StyledButton className={props.className} onClick={props.onClick}>{props.children}</StyledButton>
+}
+
+export default Button;
